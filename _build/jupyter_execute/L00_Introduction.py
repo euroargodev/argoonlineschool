4eeace53b57c4752503643009a326a0473dd70da
Pedro Velez <pvelezbelchi@gmail.com>
2021-08-30 22:48:32 +0100

    minor tweaks after upgrade to the new version of JB

--- _build/jupyter_execute/L00_Introduction.py
+++ _build/jupyter_execute/L00_Introduction.py
@@ -5,7 +5,9 @@
 # 
 # # Argo Online School
 # 
-# The Argo Online school is a set of videos and hands-on jupyter notebooks to describe the minimum requiremets to use, and understand, the Argo data. You can access the content as here in a [JubyterBook](https://euroargodev.github.io/argoonlineschool), or download them and use in your local machine. In that case, you should create a python enviroment to use these notebooks. To create, and activate, the enviroment AOS 
+# The Argo Online school is a set of videos and hands-on jupyter notebooks to describe the minimum requiremets to use and understand, the Argo data. 
+# 
+# You can access the hands-on content here in a [JubyterBook](https://euroargodev.github.io/argoonlineschool) or download it and use in your local machine. In that case, you should create a python enviroment to use these notebooks. To create and activate the AOS enviroment
 # 
 # ```
 # conda env create -f environment.yml
@@ -20,21 +22,20 @@
 # pip install seawater
 # ```
 # 
-# 
-# This enviroment already includes argopy stable version.
+# This enviroment already includes the last stable version of [argopy](https://argopy.readthedocs.io/en/latest/), the python library for Argo data.
 # 
 # ## Data
-# If you download the notebooks you should create a ./Data* folder where include the files used for the examples. The files in the Data folder can be dowloaded from [here](https://www.oceanografia.es/argoonlineschool/Data/) or directly from its source using _wget_:
+# If you download the notebooks you should create a ./Data* folder to include the files used for the examples. The files in the Data folder can be dowloaded from [here](https://www.oceanografia.es/argoonlineschool/Data/) or directly from its source using _wget_, for instance:
 # 
-# Daily NOAA OI SST V2 High Resolution Dataset for 2009:
+# for the *Daily NOAA OI SST V2 High Resolution Dataset for 2009*:
 # 
 # `! wget ftp://ftp2.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.2019.nc`
 # 
-# Data from float 6900772
+# for the data from float 6900772:
 # 
 # `wget ftp://ftp.ifremer.fr/ifremer/argo/dac/coriolis/6900772/*`
 # 
-# Data in the north Atlantic for the 11th November 2019
+# or for the data in the north Atlantic for the 11th November 2019
 # 
 # `! wget ftp://ftp.ifremer.fr/ifremer/argo//geo/indian_ocean/2019/11/20191111_prof.nc`
 # 
@@ -47,14 +48,15 @@
 # More information can be found in the [Argo Steering Team web page](http://www.argo.ucsd.edu/) or the [Argo Data Management team documentation](http://www.argodatamgt.org/Documentation)
 # 
 
-# **Before proceding, let's check if you really need this course, and try to answer the following questions**
+# **Before proceding, let's check if you really need this course** 
+# Try to answer the following questions**
 # 
 # - what is a WMO number ?
 # - what is the difference between Delayed and Real Time data mode ?
 # - what is an adjusted parameter ?
 # - what a QC flag of 3 means ?
 # 
-# If you don’t answer to more than 1 questions, please keep reading (or watching). Even though if you answered all the questions but you ara not very familiar with python, netCDF, or argopy, you may find interesting the content of this course
+# If you don’t answer to more than 1 questions, please keep reading (or watching). Even though if you answered all the questions but you are not very familiar with python, netCDF, or argopy, you may find interesting the content of this course.
 
 # In[ ]:
 
