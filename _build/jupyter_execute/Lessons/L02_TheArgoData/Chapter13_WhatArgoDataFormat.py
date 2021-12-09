a5ed6deb79a26945fddb2a396630e1266ab73fda
Pedro Velez <pvelezbelchi@gmail.com>
2021-12-09 10:05:15 +0000

    Update the path to the /Data folder

--- _build/jupyter_execute/Lessons/L02_TheArgoData/Chapter13_WhatArgoDataFormat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# #   What are the Argo data and its format?
-
-# **[HOST - Video 203-01] What are the Argo data and its format? INT. (White background) [Animations]: data figures in the background. Visually explain the data format with examples as the hosts describe them.**
-# 
